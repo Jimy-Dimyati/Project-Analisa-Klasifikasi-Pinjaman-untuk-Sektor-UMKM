05c3f5bbfeafa4b5ba76ee6398aba33d5923a137
Jimy-Dimyati <113796534+Jimy-Dimyati@users.noreply.github.com>
2024-05-20 09:46:05 +0700

    Pemilihan data kategori.r

---
+++ Pemilihan data kategori.r
@@ -0,0 +1,7 @@
+data_kategorik = data_reduce[,c("KONDISI_USAHA","KONDISI_JAMINAN","REKOMENDASI_TINDAK_LANJUT")]
+
+data_reduce$REKOMENDASI_TINDAK_LANJUT = as.factor(data_reduce$REKOMENDASI_TINDAK_LANJUT)
+
+chisq.test(data_kategorik$KONDISI_USAHA, data_kategorik$REKOMENDASI_TINDAK_LANJUT)
+
+chisq.test(data_kategorik$KONDISI_JAMINAN, data_kategorik$REKOMENDASI_TINDAK_LANJUT)
